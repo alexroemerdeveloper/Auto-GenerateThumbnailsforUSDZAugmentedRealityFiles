2f7879be719e20dd2167c8a9deeef44a9db4975d
Alexander Römer <alex@roemer.design>
2020-10-18 15:55:24 +0200

    Initial Commit

---
+++ Auto-GenerateThumbnailsforUSDZAugmentedRealityFiles/Auto_GenerateThumbnailsforUSDZAugmentedRealityFilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Auto_GenerateThumbnailsforUSDZAugmentedRealityFilesApp.swift
+//  Auto-GenerateThumbnailsforUSDZAugmentedRealityFiles
+//
+//  Created by Alexander Römer on 18.10.20.
+//
+
+import SwiftUI
+
+@main
+struct Auto_GenerateThumbnailsforUSDZAugmentedRealityFilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
